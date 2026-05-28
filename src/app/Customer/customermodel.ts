ff2c1530bf4cbda314e21112187de2d90aeb34a2
Benson007-star <bensonbenaiah2006@gmail.com>
2026-05-28 11:00:07 +0530

    first commit

---
+++ src/app/Customer/customermodel.ts
@@ -0,0 +1,8 @@
+
+export interface Customermodel {
+  id: any;
+  customerType: string;
+  fullName: string;
+  email: string;
+  phone: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Hello } from './app/hello/hello';
+import { Customer } from './app/Customer/customer';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Customer, appConfig)
   .catch((err) => console.error(err));
